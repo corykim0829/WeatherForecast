96f340542b91ce3cc0cf54155b5c257297e7b0b7
corykim0829 <corykim0829@gmail.com>
2024-07-09 13:59:39 +0900

    프로젝트 생성

---
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Cory Kim on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
